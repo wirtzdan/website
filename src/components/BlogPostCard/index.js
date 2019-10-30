3c909e1f84942fb3565835ce7884603cf0212822
Daniel Wirtz <danielwirtzx@gmail.com>
2019-10-30 23:36:35 +0100

    :lipstick: Style tag component

--- src/components/BlogPostCard/index.js
+++ src/components/BlogPostCard/index.js
@@ -6,12 +6,14 @@ function BlogPostCard({ title, subtitle, timeToRead, slug }) {
     <Link className="md:w-1/2 w-1/1 h-65 p-2" to={slug}>
       <article className="h-full flex flex-col justify-between items-start bg-white dark:bg-neutral-700 rounded-lg p-6 shadow">
         <div>
-          <h2 className="text-3xl leading-tight text font-bold mb-3">{title}</h2>
+          <h2 className="text-3xl leading-tight text font-bold mb-3">
+            {title}
+          </h2>
           <p className="text-xl text-neutral-500 dark:text-neutral-200 leading-tight">
             {subtitle}
           </p>
         </div>
-        <button className="text px-3 py-1 bg-blue-100 text-blue-600 rounded-lg uppercase font-bold mt-6">
+        <button className="text-sm px-3 py-1 bg-primary-50 text-primary-600 rounded-lg uppercase font-semibold mt-2 dark:bg-primary-600 dark:text-primary-50">
           {timeToRead} minute
         </button>
       </article>
