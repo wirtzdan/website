0828e678e6abdd4ad15dcda5eb57c12bc969a4ee
Daniel Wirtz <danielwirtzx@gmail.com>
2019-10-31 22:53:59 +0100

    :lipstick: Change margin

--- src/components/common/section.js
+++ src/components/common/section.js
@@ -3,7 +3,7 @@ import Container from "./container";
 
 function Section({ children, ...props }) {
   return (
-    <section className="my-10">
+    <section className="mt-10">
       <Container {...props}>{children}</Container>
     </section>
   );
