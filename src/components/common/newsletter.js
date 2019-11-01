9394667bff00954c863956008357118b3eeca51c
Daniel Wirtz <danielwirtzx@gmail.com>
2019-11-01 20:36:11 +0100

    :sparkles: Integerate component with Mailchimp

--- src/components/common/newsletter.js
+++ src/components/common/newsletter.js
@@ -1,43 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Section from "./section";
 import { Button } from "./index";
+import addToMailchimp from "gatsby-plugin-mailchimp";
 
 function Newsletter() {
+  const [state, setState] = useState({ mail: "", status: "", msg: "" });
+
+  const handleChange = event => {
+    setState({ mail: event.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    addToMailchimp(state.mail)
+      .then(({ msg, result }) => {
+        if (result !== "success") {
+          setState({ status: "success" });
+          setState({ msg: msg });
+          throw msg;
+        }
+      })
+      .catch(err => {
+        setState({ status: "error" });
+        setState({ msg: err });
+        console.log(state);
+      });
+  };
+
   return (
     <Section extend="mt-0">
-      <div className="bg-green-500 px-8 py-12 rounded-lg">
-        <div class="text-white">
+      <div className="bg-green-500 p-12 rounded-lg">
+        <div class="text-white measure-narrow">
           <h3>Don't want to miss a post?</h3>
           <p>
             Join my personal email list and get notified for new posts and fresh
             thoughts.
           </p>
         </div>
-        <div className="flex flex-wrap items-end -mx-2 overflow-hidden">
-          <label className="my-2 px-2 md:w-1/4">
-            <span className="text-xl text-neutral-50">First Name</span>
+        <form
+          className="flex flex-wrap items-end -mx-2 overflow-hidden"
+          onSubmit={handleSubmit}
+        >
+          <label className="my-2 w-full px-2 md:w-2/4">
             <input
-              class="bg-white focus:outline-none text-gray-800 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal w-full"
+              className="bg-white focus:outline-none text-neutral-600 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal w-full"
+              name="email"
               type="text"
-              placeholder="Hans"
-            ></input>
-          </label>
-          <label className="my-2 px-2 md:w-1/2">
-            <span className="text-xl text-neutral-50">E-Mail</span>
-            <input
-              className="bg-white focus:outline-none text-gray-800  focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal w-full"
-              type="email"
-              placeholder="hansdown@gmail.com"
+              placeholder="me@email.com"
+              onChange={handleChange}
+              value={state.mail}
             ></input>
           </label>
           <div className="my-4 md:my-2 px-2 w-full md:w-1/4">
-            <Button
-              text="Subscribe"
-              to="test"
-              extend="w-full bg-white text-green-700"
-            ></Button>
+            <button
+              type="submit"
+              className="inline-flex items-center justify-center py-3 px-6 rounded-lg font-bold text-xl text-white h-full w-full bg-white text-green-700"
+            >
+              Subscribe
+            </button>
           </div>
-        </div>
+        </form>
+        <div className="text-white my-2 text-base ">{state.msg}</div>
       </div>
     </Section>
   );
