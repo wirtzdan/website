f5c0b859a5d0d0d31a393c58f020819824fc5b27
Daniel Wirtz <danielwirtzx@gmail.com>
2019-11-02 10:05:15 +0100

    :sparkles: Add dark transition

--- src/components/common/layout.js
+++ src/components/common/layout.js
@@ -6,7 +6,7 @@ import MDHeader from "../MobileMenu";
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col font-body min-h-screen text-neutral-700 dark:text-neutral-100 antialiased bg-neutral-50 dark:bg-neutral-900">
+    <div className="flex flex-col font-body min-h-screen text-neutral-700 dark:text-neutral-100 antialiased bg-neutral-50 dark:bg-neutral-900 dark-transition">
       <Header />
       <MDHeader />
       <main className="flex flex-col flex-1 mx-auto w-full mb-20 md:mb-6">
