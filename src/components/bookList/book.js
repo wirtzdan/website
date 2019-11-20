1951b942e9d25a36282c27e638043f88be87048c
Daniel Wirtz <danielwirtzx@gmail.com>
2019-11-20 23:20:37 +0100

    :lipstick: Add realistic style to books

--- src/components/bookList/book.js
+++ src/components/bookList/book.js
@@ -3,13 +3,14 @@ import { motion } from "framer-motion";
 
 function Book(props) {
   return (
-    <div className="md:w-1/4 my-2 px-2 w-1/2">
-      <a href={props.link} className="w-full h-full">
-        <motion.img
+    <div className="relative md:w-1/4 my-2 px-2 w-1/2">
+      <div class="ml-2 book-fold absolute w-4 inset-y-0 left-0 z-10"></div>
+      <a href={props.link} className="w-full h-full relative">
+        <img
           whileHover={{ scale: 1.015, rotate: 2 }}
-          className="rounded-lg shadow-md hover:shadow-lg w-full h-full object-cover"
+          className="rounded-sm rounded-r-lg shadow-lg hover:shadow-lg w-full h-full object-cover"
           src={props.imageUrl}
-        ></motion.img>
+        ></img>
       </a>
     </div>
   );
