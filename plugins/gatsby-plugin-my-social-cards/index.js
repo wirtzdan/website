9c44503937b04eb5b1e54ae03c0b3325f6e3f756
Daniel Wirtz <danielwirtzx@gmail.com>
2019-12-23 00:48:33 +0100

    :bento: Change font files

--- plugins/gatsby-plugin-my-social-cards/index.js
+++ plugins/gatsby-plugin-my-social-cards/index.js
@@ -10,8 +10,8 @@ module.exports = ({ markdownNode }) => {
 
   return Promise.all([
     jimp.read(path.join(__dirname, "base.jpg")),
-    jimp.loadFont(path.join(__dirname, "fonts/rhd-80.fnt"))
-  ]).then(([image, rh80]) => {
+    jimp.loadFont(path.join(__dirname, "fonts/rhd-120.fnt"))
+  ]).then(([image, rhd120]) => {
     const WIDTH = 1200;
     const HEIGHT = 630;
     const PADDING = 40;
