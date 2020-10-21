3573aa0cb05a26a0980a466b794719b53881afae
Daniel Wirtz <danielwirtzx@gmail.com>
2020-10-21 09:32:04 +0200

    :lipstick: Add padding around blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -37,7 +37,7 @@ class BlogPostTemplate extends React.Component {
           />
 
           <Section>
-            <article className="px-6 bg-white rounded shadow md:px-12 md:py-4 markdown dark:bg-neutral-700" ref={target}>
+            <article className="px-6 pb-4 bg-white rounded shadow md:px-12 md:py-4 markdown dark:bg-neutral-700" ref={target}>
               <header>
                 <h1 className="text-5xl">{post.frontmatter.title}</h1>
               </header>
