63c1077ae0a90a31eddc2f5a1be35ec754f717f2
Daniel Wirtz <danielwirtzx@gmail.com>
2020-12-20 13:00:00 +0100

    :sparkles: Add custom theme

---
+++ theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const customTheme = extendTheme({
+  fonts: {
+    heading: "Red Hat Display, sans-serif",
+    body: "Red Hat Text, sans-serif",
+  },
+});
+
+export default customTheme;
