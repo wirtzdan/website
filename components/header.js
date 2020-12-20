4c4fa0dd72009c3a43c203b0889e049cd499deb0
Daniel Wirtz <danielwirtzx@gmail.com>
2020-12-20 21:13:38 +0100

    :sparkles: Add header component

--- components/header.js
+++ components/header.js
@@ -7,7 +7,9 @@ import {
   IconButton,
   useColorMode,
   Text,
+  Box,
   Divider,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 import Container from "./container";
@@ -20,10 +22,20 @@ function NavLink(props) {
 
   const [, group] = href.split("/");
   const isActive = pathname.includes(group);
+  console.log("🚀 ~ file: header.js ~ line 24 ~ NavLink ~ isActive", isActive);
 
   return (
     <NextLink href={href} passHref>
-      <Button variant="ghost" {...rest}>
+      <Button
+        aria-current={isActive ? "page" : undefined}
+        variant="ghost"
+        size="md"
+        {...rest}
+        _activeLink={{
+          color: useColorModeValue("blue.500", "blue.200"),
+        }}
+        px={4}
+      >
         {name}
       </Button>
     </NextLink>
@@ -32,26 +44,32 @@ function NavLink(props) {
 
 const Header = () => {
   return (
-    <Container>
-      <VStack align="start" spacing={4} display={{ base: "none", md: "flex" }}>
-        <NextLink href="/" passHref>
-          <chakra.a display="block" aria-label="Home">
-            <Text fontSize="3xl" fontWeight="bold">
-              Daniel Wirtz
-            </Text>
-          </chakra.a>
-        </NextLink>
-        <HStack justify="space-between" w="100%">
-          <HStack ml={-4}>
-            <NavLink href="/blog" name="Blog" />
-            <NavLink href="/about" name="About" />
-            <NavLink href="/books" name="Books" />
+    <Box
+      bg={useColorModeValue("white", "gray.800")}
+      display={{ base: "none", md: "block" }}
+    >
+      <Container py={2}>
+        <VStack align="start" spacing={0}>
+          {/* <NextLink href="/" passHref>
+            <chakra.a display="block" aria-label="Home">
+              <Text fontSize="2xl" fontWeight="bold">
+                Daniel Wirtz
+              </Text>
+            </chakra.a>
+          </NextLink> */}
+          <HStack justify="space-between" w="100%">
+            <HStack ml={-4} spacing={2}>
+              <NavLink href="/" name="Home" />
+              <NavLink href="/about" name="About" />
+              <NavLink href="/blog" name="Blog" />
+              <NavLink href="/books" name="Books" />
+            </HStack>
+            <ThemeToggle />
           </HStack>
-          <ThemeToggle />
-        </HStack>
-        <Divider />
-      </VStack>
-    </Container>
+        </VStack>
+      </Container>
+      <Divider />
+    </Box>
   );
 };
 export default Header;
