0c47c3507c326b4de172a12e2ed1b8e90f545615
Daniel Wirtz <danielwirtzx@gmail.com>
2020-12-20 21:14:24 +0100

    :pencil: Add font

--- theme.js
+++ theme.js
@@ -1,9 +1,26 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 const customTheme = extendTheme({
   fonts: {
-    heading: "Red Hat Display, sans-serif",
-    body: "Red Hat Text, sans-serif",
+    heading: "'Red Hat Display', sans-serif",
+    body: "'Red Hat Text', sans-serif",
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        color: mode("gray.700", "whiteAlpha.900")(props),
+        bg: mode("gray.50", "gray.900")(props),
+        ".deleted": {
+          color: "#ff8383 !important",
+          fontStyle: "normal !important",
+        },
+        ".inserted": {
+          color: "#b5f4a5 !important",
+          fontStyle: "normal !important",
+        },
+      },
+    }),
   },
 });
 
