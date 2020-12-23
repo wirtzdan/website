c3e58408fc822e1aa93f892a9eacb3296c0a3f48
Daniel Wirtz <danielwirtzx@gmail.com>
2020-12-24 00:01:23 +0100

    :lipstick: Add footer padding

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { HStack, Button, useColorMode, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Button,
+  useColorMode,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import Container from "../components/container";
 import NextLink from "next/link";
 
@@ -11,7 +17,12 @@ const Footer = () => {
 
     return (
       <NextLink href={href} passHref>
-        <Button variant="unstyled" {...rest}>
+        <Button
+          variant="unstyled"
+          {...rest}
+          color={useColorModeValue("gray.500", "gray.600")}
+          _hover={{ color: useColorModeValue("gray.600", "gray.500") }}
+        >
           {name}
         </Button>
       </NextLink>
@@ -24,8 +35,11 @@ const Footer = () => {
         justify="space-between"
         w="100%"
         display={{ base: "none", md: "flex" }}
+        py={4}
       >
-        <Text>© {date} Daniel Wirtz </Text>
+        <Text fontSize="sm" color={useColorModeValue("gray.500", "gray.600")}>
+          © {date} Daniel Wirtz{" "}
+        </Text>
         <HStack spacing={4}>
           <FooterLink href="/disclaimer" name="Disclaimer" />
           <FooterLink href="/privacy" name="Privacy" />
