293856f591ce338a5474f88ff508ad2ebf643d66
Daniel Wirtz <danielwirtzx@gmail.com>
2020-12-23 01:32:39 +0100

    :construction:

--- pages/api/sendSuggestion.js
+++ pages/api/sendSuggestion.js
@@ -8,36 +8,33 @@ export default function handler(req, res) {
   // Parse stringified json
   const body = JSON.parse(req.body);
 
-  return new Promise((resolve, reject) => {
-    sendSuggestion(body)
-      .then((response) => {
-        res.status(200).json(response);
-        resolve();
-      })
-      .catch((error) => {
-        res.json(error);
-        res.status(405);
-        reject;
-      });
-  });
+  sendSuggestion(body)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(405).json(error);
+    });
 
   function sendSuggestion(data) {
     const { title, author, message } = data;
     const table = base("Suggestions");
 
-    table.create(
-      {
-        Title: title,
-        Author: author,
-        Message: message,
-      },
-      function (err, record) {
-        if (err) {
-          console.error(err);
-          return;
+    return new Promise((resolve, reject) => {
+      table.create(
+        {
+          Title: title,
+          Author: author,
+          Message: message,
+        },
+        function (err, record) {
+          if (err) {
+            console.error(err);
+            reject();
+          }
+          resolve();
         }
-        console.log(record.get("Title"));
-      }
-    );
+      );
+    });
   }
 }
