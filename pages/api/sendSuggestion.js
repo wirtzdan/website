f27767e42fb0cddfc4de6084a80d85be7a5ef2ef
Daniel Wirtz <danielwirtzx@gmail.com>
2020-12-23 01:21:35 +0100

    :construction: Suggestion API

--- pages/api/sendSuggestion.js
+++ pages/api/sendSuggestion.js
@@ -4,41 +4,39 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_BASE_ID
 );
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   // Parse stringified json
   const body = JSON.parse(req.body);
 
   return new Promise((resolve, reject) => {
     sendSuggestion(body)
       .then((response) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.setHeader("Cache-Control", "max-age=180000");
-        res.end(JSON.stringify(response));
+        res.status(200).json(response);
         resolve();
       })
       .catch((error) => {
         res.json(error);
-        res.status(405).end();
-        return resolve(); //in case something goes wrong in the catch block (as vijay) commented
+        res.status(405);
+        reject;
       });
   });
 
-  async function sendSuggestion(data) {
+  function sendSuggestion(data) {
     const { title, author, message } = data;
     const table = base("Suggestions");
 
-    return table.create(
+    table.create(
       {
         Title: title,
         Author: author,
         Message: message,
       },
-      function (err) {
+      function (err, record) {
         if (err) {
           console.error(err);
           return;
         }
+        console.log(record.get("Title"));
       }
     );
   }
