b18aa717c0f85701ff877dac2e87f4897262a4c9
Daniel Wirtz <danielwirtzx@gmail.com>
2020-12-31 11:52:50 +0100

    :sparkles: Add new favicons

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,37 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&family=Red+Hat+Text:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
           />
+          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="/static/favicons/site.webmanifest" rel="manifest" />
+          <link
+            href="/static/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/static/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/static/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <link
+            color="#3182CE"
+            href="/static/favicons/safari-pinned-tab.svg"
+            rel="mask-icon"
+          />
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta content="#3182CE" name="theme-color" />
+          <meta content="#3182CE" name="msapplication-TileColor" />
+          <meta
+            content="/static/favicons/browserconfig.xml"
+            name="msapplication-config"
+          />
         </Head>
         <body>
           <Main />
