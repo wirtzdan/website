eb250f230b9e1463f33e5f557862870f3bd0555e
Daniel Wirtz <danielwirtzx@gmail.com>
2021-01-02 18:34:35 +0100

    :sparkles: Add author card and twitter button

---
+++ components/author-card.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Box,
+  HStack,
+  Avatar,
+  VStack,
+  Text,
+  Button,
+  useClipboard,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { format } from "timeago.js";
+import {
+  ClipboardCheckOutline,
+  ClipboardCopyOutline,
+  Duplicate,
+  Link,
+  Check,
+} from "heroicons-react";
+
+const AuthorCard = ({ readingTime, publishedAt, url }) => {
+  const { hasCopied, onCopy } = useClipboard(url);
+
+  return (
+    <Stack direction="row" justify="space-between">
+      <HStack>
+        <Avatar src="/avatar-small.jpg" h={10} w={10}></Avatar>
+        <VStack spacing={0} align="start">
+          <Text fontSize="md" fontWeight="500">
+            Daniel Wirtz
+          </Text>
+          <Text fontSize="sm" color={useColorModeValue("gray.500", "gray.200")}>
+            {format(publishedAt)} • {readingTime}
+          </Text>
+        </VStack>
+      </HStack>
+      <HStack>
+        <Button
+          onClick={onCopy}
+          ml={2}
+          variant="outline"
+          size={["sm"]}
+          color={
+            hasCopied
+              ? useColorModeValue("green.600", "green.200")
+              : useColorModeValue("gray.800", "gray.100")
+          }
+          bg={useColorModeValue("white", "gray.800")}
+          leftIcon={hasCopied ? <Check size={18} /> : <Link size={18} />}
+        >
+          {hasCopied ? "Copied" : "Copy link"}
+        </Button>
+      </HStack>
+    </Stack>
+  );
+};
+
+export default AuthorCard;
--- layouts/blog.js
+++ layouts/blog.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Section from "@/components/section";
-import { Heading, VStack, Text, HStack } from "@chakra-ui/react";
+import {
+  Heading,
+  VStack,
+  Text,
+  HStack,
+  Box,
+  Button,
+  Link,
+} from "@chakra-ui/react";
 import ReadingProgress from "@/components/reading-progress";
 import BlogSeo from "@/components/blog-seo";
+import AuthorCard from "@/components/author-card";
+import Image from "next/image";
+import { TwitterLogo } from "phosphor-react";
+import { ArrowNarrowRight } from "heroicons-react";
 
 class BlogLayout extends React.Component {
   render() {
     const target = React.createRef();
 
-    console.log(
-      "🚀 ~ file: blog.js ~ line 20 ~ BlogLayout ~ render ~ this.props",
-      this.props
-    );
+    const discussUrl = (slug) =>
+      `https://mobile.twitter.com/search?q=${encodeURIComponent(
+        `https://danielwirtz.com/blog/${slug}`
+      )}`;
 
     return (
       <>
@@ -21,11 +33,47 @@ class BlogLayout extends React.Component {
             {...this.props.frontMatter}
           />
           <article ref={target}>
-            <VStack w="100%" align="left">
-              <Heading as="h1">{this.props.frontMatter.title}</Heading>
+            <VStack w="100%" align="left" spacing={6}>
+              <Box
+                mt={4}
+                rounded="lg"
+                shadow="md"
+                overflow="hidden"
+                lineHeight={0}
+              >
+                {" "}
+                <Image
+                  alt={this.props.frontMatter.title}
+                  src={this.props.frontMatter.image}
+                  width={2240}
+                  height={1260}
+                />
+              </Box>
+
+              <VStack align="stretch" spacing={4}>
+                <Heading as="h1">{this.props.frontMatter.title}</Heading>
+                <AuthorCard
+                  publishedAt={this.props.frontMatter.publishedAt}
+                  url={
+                    "https://danielwirtz.com/blog/" +
+                    this.props.frontMatter.slug
+                  }
+                  readingTime={this.props.frontMatter.readingTime.text}
+                />
+              </VStack>
             </VStack>
             <div>{this.props.children}</div>
           </article>
+          <Link href={discussUrl(this.props.frontMatter.slug)} isExternal>
+            <Button
+              leftIcon={<TwitterLogo weight="fill" />}
+              colorScheme="blue"
+              mt={6}
+            >
+              Discuss on Twitter
+            </Button>
+          </Link>
+
           <div ref={(el) => (this.div = el)}></div>
         </Section>
         <ReadingProgress target={target} />
