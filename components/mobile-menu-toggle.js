080709f03ae2a337cc0c588eeab48653d424a3e6
Daniel Wirtz <danielwirtzx@gmail.com>
2021-02-02 09:01:40 +0100

    :bug: Fix button text leaking out

--- components/mobile-menu-toggle.js
+++ components/mobile-menu-toggle.js
@@ -62,9 +62,9 @@ const MobileMenuToggle = ({ mobile }) => {
                 <MobileMenuItem href="/" title="Home" />
                 <MobileMenuItem href="/about" title="About" />
                 <MobileMenuItem href="/blog" title="Blog" />
-                <SimpleGrid columns={3} spacing={2}>
+                <MobileMenuItem href="/bookmarks" title="Bookmarks" />
+                <SimpleGrid columns={2} spacing={2} w="100%">
                   <MobileMenuItem href="/books" title="Books" />
-                  <MobileMenuItem href="/bookmarks" title="Bookmarks" />
                   <MobileMenuItem href="/tools" title="Tools" />
                 </SimpleGrid>
               </VStack>
