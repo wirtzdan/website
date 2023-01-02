8ed850b76a9e10e78c78823b94d7d4b7d5fd4f8c
Daniel Wirtz <daniel@crisp.studio>
2023-01-02 22:06:46 +0100

    :sparkles: Simpler home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import fs from "fs";
 import Head from "next/head";
 import {
   Button,
@@ -7,84 +8,66 @@ import {
   Text,
   Heading,
   SimpleGrid,
+  Stack,
+  HStack,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import PageTransition from "../components/page-transitions";
 import Section from "@/components/section";
-import { TwitterLogo } from "phosphor-react";
-import ProjectCard from "@/components/project-card";
-import { getTable } from "@/lib/airtable";
+import sorter from "sort-isostring";
+import { getAllPosts } from "../lib/airtable";
 import Link from "@/components/link";
 import SubscribeCard from "@/components/subscribe-card";
+import BlogListItem from "@/components/blog-list-item";
+import generateRssIcon from "@/lib/rss";
+import Subscribe from "@/components/subscribe";
 
-const Home = ({ projects }) => (
+const Home = ({ posts }) => (
   <Box>
     <PageTransition>
       <VStack spacing={12}>
         <Section>
           <VStack spacing={4} align="start" fontSize="2xl">
-            <Heading size="xl">Hey, I'm Daniel 👋</Heading>
-            <VStack>
-              <Text>
-                I'm a designer, developer and entrepreneur of sorts. Born and
-                raised in Germany and now living in the Netherlands. The focus
-                of my work is the{" "}
+            <VStack
+              w="100%"
+              // spacing={8}
+              rounded="lg"
+              borderWidth="1px"
+              bg={useColorModeValue("white", "neutralD.100")}
+              borderColor={useColorModeValue("neutral.400", "neutralD.400")}
+              p={8}
+              align="start"
+              // height={48}
+            >
+              <Heading size="lg">Hey, I'm Daniel. </Heading>
+              <Text fontSize={["lg", "2xl"]}>
+                I’m a designer, facilitator, creator and the co-founder of{" "}
                 <Link variant="text" href="https://www.facilitator.school">
                   Facilitator School
                 </Link>
-                . In my spare time, I work on{" "}
-                <Link variant="text" href="https://markway.io">
-                  Markway
-                </Link>
-                . I also hang out on{" "}
-                <Link variant="text" href="https://twitter.com/wirtzdan">
-                  Twitter
-                </Link>{" "}
-                and{" "}
-                <Link
-                  variant="text"
-                  href="https://www.linkedin.com/in/wirtzdan/"
-                >
-                  LinkedIn
-                </Link>
-                , where I learn, think and work in public.
+                . This is my corner of the internet.
               </Text>
             </VStack>
-            {/* <Link href="https://twitter.com/wirtzdan" unstyled>
-              <Button
-                colorScheme="blue"
-                rounded="lg"
-                size="lg"
-                leftIcon={<TwitterLogo weight="fill" />}
-                mt={4}
-              >
-                Follow me on Twitter
-              </Button>
-            </Link> */}
           </VStack>
         </Section>
-        <Section>
-          <SubscribeCard
-            title="Subscribe to my newsletter"
-            description="Helpful tools, thoughtful articles and other findings from the web. From my desk to yours."
-          />
-        </Section>
-
         <Section>
           <VStack align="start" spacing={8}>
-            <Heading size="lg">Projects</Heading>
+            <Heading size="lg">Recent Posts</Heading>
             <SimpleGrid columns={1} spacing={4} mt={8} w="100%">
-              {projects.map((projects) => (
-                <ProjectCard
-                  key={projects.id}
-                  name={projects.fields.name}
-                  description={projects.fields.description}
-                  logo={projects.fields.logo}
-                  link={projects.fields.link}
-                  type={projects.fields.type}
-                />
-              ))}
+              {posts
+                .slice(0, 3)
+                .filter((p) => p.fields.status === "Published")
+                .sort((x, y) =>
+                  sorter(y.fields.publishDate, x.fields.publishDate)
+                )
+                .map((post) => {
+                  return <BlogListItem key={post.id} {...post.fields} />;
+                })}
             </SimpleGrid>
           </VStack>
+        </Section>
+        <Section>
+          <SubscribeCard title="Subscribe to my newsletter" description="" />
         </Section>
       </VStack>
     </PageTransition>
@@ -92,13 +75,17 @@ const Home = ({ projects }) => (
 );
 
 export async function getStaticProps() {
-  const projects = await getTable("Projects");
+  const posts = await getAllPosts();
+
+  const rss = await generateRssIcon(posts);
+
+  fs.writeFileSync("./public/rss.xml", rss);
 
   return {
     props: {
-      projects,
+      posts,
     },
-    revalidate: 600,
+    revalidate: 10,
   };
 }
 
